55fecc7501fce2f767d2d402bf80241d6fcd25dc
kaisheng deng <carmen.deng12@gmail.com>
2022-03-03 22:34:40 -0500

    phase 2 complete

--- server.js
+++ server.js
@@ -2,9 +2,36 @@ const express = require('express')
 const {syncAndSeed,models:{Trainer,Pokemon,Catches},db} = require('./db');
 const app = express();
 const path = require('path')
-app.use('/src',express.static(path.join(__dirname,'src')))
+// app.use('/src',express.static(path.join(__dirname,'src')))
 
-app.use('/api',require('./api'));
+app.get('/api/trainers',async(req,res,next)=>{
+    try{
+        res.send(await Trainer.findAll())
+    }catch(err){
+        next(err);
+    }
+})
+
+app.get('/api/pokemons',async(req,res,next)=>{
+    try{
+        res.send(await Pokemon.findAll())
+    }catch(err){
+        next(err);
+    }
+})
+
+app.get('/api/trainers/:id/catches',async(req,res,next)=>{
+    try{
+        res.send(await Catches.findAll({
+            where:{
+                trainerId: req.params.id
+            },
+            include:[Pokemon]
+        }))
+    }catch(err){
+        next(err);
+    }
+})
 
 app.get('/',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'index.html'))
